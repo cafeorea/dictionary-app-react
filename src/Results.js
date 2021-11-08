9670025213150d859be49111029f23dc7171f1e2
lincodes <83891163+lincodes@users.noreply.github.com>
2021-11-08 19:49:07 +0900

    Implemented phonetic audio and text

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   console.log(props.results);
@@ -7,6 +8,13 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
