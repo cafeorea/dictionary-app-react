2848e8318be339c12014563c7aef72dc2da89ee3
lincodes <83891163+lincodes@users.noreply.github.com>
2021-11-11 20:48:50 +0900

    Image API integration
    Integrated pexel API, also made the book icon on the header refresh the page when clicked.

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./Header.css";
 
 export default function Header() {
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
   return (
     <nav className="navbar  navbar-light navigation">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 navtext">📖</span>
+        <span className="navbar-brand mb-0 h1 navtext" onClick={refreshPage}>
+          📖
+        </span>
       </div>
     </nav>
   );
--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Search.css";
 
 export default function Search() {
   let [searchTerm, setSearchTerm] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
     setResults(response.data[0]);
+  }
+
+  function pexelsSearch(response) {
+    setPhotos(response.data.photos);
   }
 
   function dictionarySearch(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000019c3d31bb4fc949b8afd44f5ad2a4c057";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchTerm}`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(pexelsSearch);
   }
 
   function handleSearchTerm(event) {
@@ -37,6 +51,7 @@ export default function Search() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
